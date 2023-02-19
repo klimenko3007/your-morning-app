03dcd41af02764745f2ce70970b771297637b7fd
klimenko3007 <ek1830@tobii.com>
2023-02-19 20:09:37 +0100

    Installed PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 import legacy from '@vitejs/plugin-legacy';
 import { fileURLToPath } from 'url';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react()],
+  plugins: [
+    legacy(),
+    VitePWA({ registerType: 'autoUpdate' }),
+    { ...react(), enforce: 'pre' },
+  ],
   server: {
     port: 4000,
     strictPort: true,
